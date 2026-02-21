2bd3a3b4d691f94239ef5fd5e7e410f3648e8fda
kishu1084 <patelkrishna01062003@gmail.com>
2026-02-21 13:01:58 +0530

    Added Light Gradient Boosting Machine Classifier

---
+++ LGBM/LGBMClassifier-poly-06.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('diabetes_data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:15])
+X[:, 1:15] = imputer.transform(X[:, 1:15])
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.6, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#PCA
+from sklearn.decomposition import PCA, KernelPCA
+
+kernel_pca = KernelPCA(
+    n_components=5,          # reduce dimensionality
+    kernel="poly",
+    degree=3,
+    gamma=None,              # let sklearn choose 1/n_features
+    coef0=1,
+    fit_inverse_transform=False   # VERY IMPORTANT
+)
+
+X_train_kpca = kernel_pca.fit_transform(X_train)
+X_test_kpca = kernel_pca.transform(X_test)
+
+
+#LGBM
+from lightgbm import LGBMClassifier
+classifier = LGBMClassifier()
+
+
+
+classifier.fit(X_train_kpca, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test_kpca)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+
+from sklearn.metrics import classification_report
+
+
+print(classification_report(y_test, y_pred))
+
+# True Positive (TP): we predict a label of 1 (positive), and the true label is 1.
+tp = np.sum(np.logical_and(y_test == 1, y_pred == 1))
+ 
+# True Negative (TN): we predict a label of 0 (negative), and the true label is 0.
+tn = np.sum(np.logical_and(y_test == 0, y_pred == 0))
+ 
+# False Positive (FP): we predict a label of 1 (positive), but the true label is 0.
+fp = np.sum(np.logical_and(y_test == 0, y_pred == 1))
+ 
+# False Negative (FN): we predict a label of 0 (negative), but the true label is 1.
+fn = np.sum(np.logical_and(y_test == 1, y_pred== 0))
+
+# Sensitivity, hit rate, recall, or true positive rate
+tpr = tp/(tp+fn)
+# Specificity or true negative rate
+tnr = tn/(tn+fp) 
+# Precision or positive predictive value
+ppv = tp/(tp+fp)
+# Negative predictive value
+npv = tn/(tn+fn)
+# Fall out or false positive rate
+fpr = fp/(fp+tn)
+# False negative rate
+fnr = fn/(tp+fn)
+# False discovery rate
+fdr = fp/(tp+fp)
+
+# Overall accuracy
+acc = (tp+tn)/(tp+fp+fn+tn)
+
+if tp>0:
+  precision=float(tp)/(tp+fp)
+  recall=float(tp)/(tp+fn)
+
+print('\nConfusion matrix for LGBM:\n',confusion_matrix(y_test,  y_pred))  
+print('\nTrue Positive : %d'%(tp))
+print('\nTrue Negative : %d'%(tn))
+print('\nFalse Positive : %d'%(fp))
+print('\nFalse Negative : %d'%(fn))
+print('\nSensitivity, hit rate, recall, or true positive rate : %f' %(tpr))
+print('\nSpecificity or true negative rate : %f' %(tnr))
+print('\nPrecision or positive predictive value : %f' %(ppv))
+print('\nNegative predictive value : %f'%(npv))
+print('\nFall out or false positive rate: %f' %(fpr))
+print('\nFalse negative rate : %f' %(fnr))
+print('\nFalse discovery rate : %f' %(fdr))
+print('\nPrecision : %f' %precision)
+print('\nRecall : %f' %recall)
+print('\nOverall accuracy for Classifier : %f' %(acc))
